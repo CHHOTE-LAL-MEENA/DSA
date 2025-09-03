29cdd5c14014eda68a9c0ad25a9e649536ff3f97
CHHOTE-LAL-MEENA <chhotelalmeena372@gmail.com>
2025-09-03 22:04:07 +0530

    Initial commit

--- Apna College/30.validPalindrom.cpp
+++ Apna College/30.validPalindrom.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+bool isAlphnum(char ch)
+{
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+        return true;
+    return false;
+}
 
 bool validPalindrom(string s)
 {
     int p = 0, q = s.size() - 1;
     while (p < q)
     {
-        if (tolower(s[p++]) != tolower(s[q--]))
+        if (!isAlphnum(s[p]))
+        {
+            p++;
+            continue;
+        }
+        if (!isAlphnum(s[q]))
+        {
+            q--;
+            continue;
+        }
+        if (tolower(s[p]) != tolower(s[q]))
             return false;
+        p++;
+        q--;
     }
     return true;
 }
 int main()
 {
-    string s = "mana plana c analp anam";
+    string s = "mana52, plana :c analp, 25anam";
+    cout << s<<endl;
     cout << validPalindrom(s) << endl;
-    cout << s;
 
     return 0;
 }
